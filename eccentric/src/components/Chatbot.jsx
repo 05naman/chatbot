b03e7d34e47e533abb773087c4045180dd92d57c
Naman Nigam <namannigam30@gmail.com>
2025-02-16 16:53:43 +0530

    updated

--- eccentric/src/components/Chatbot.jsx
+++ eccentric/src/components/Chatbot.jsx
@@ -60,8 +60,7 @@ const Chatbot = () => {
     }
 
     const { latitude, longitude } = location;
-    // const apiUrl = `https://api.geoapify.com/v2/places?categories=${category}&filter=circle:${longitude},${latitude},5000&limit=5&apiKey=${GEOAPIFY_API_KEY}`;
-    const apiUrl = `https://api.geoapify.com/v2/places?categories=healthcare.hospital&filter=circle:${80.9039962},${26.8499952},5000&limit=5&apiKey=${GEOAPIFY_API_KEY}`;
+    const apiUrl = `https://api.geoapify.com/v2/places?categories=${category}&filter=circle:${longitude},${latitude},5000&limit=5&apiKey=${GEOAPIFY_API_KEY}`;
     try {
       const response = await fetch(apiUrl);
       const data = await response.json();
